0756301d49386d62c733fda280d97e0264175767
Léo Maradan <leomaradan@gmail.com>
2013-02-04 14:52:18 +0100

    Classe Mail + test live

---
+++ conf/Mail.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace conf;
+
+class Mail {
+
+	public $Bcc = array();
+	public $Cc = array();
+	public $To = array();
+	public $From;
+	public $FromName;
+	public $Reply;
+	public $ReplyName;
+	public $Subject;
+	public $MessageText;
+	public $MessageHtml;
+	private $Separator;
+	
+	public $Log;
+
+	public function __construct() {
+		$this->Separator = '-----=' . md5(uniqid(mt_rand())); 
+	}
+
+	public function Send() {
+	
+		if($this->validation()) {
+			$arrayTo = array();
+			
+			foreach($this->To as $key => $value) {
+				$arrayTo = is_numeric($key) ? $value : '"' . mb_encode_mimeheader($key) . '" <' . $value . '>';
+			}
+			
+			$to = implode(',', $arrayTo);
+		
+			$headers = $this->getHeaders();
+			
+			$msg = $this->getMessageText();
+			$msg .= $this->getMessageHtml();
+			
+			$send = mail($to, '=?UTF-8?B?' . base64_encode($this->Subject) . '?=', $msg, $headers);		
+			
+			$this->Log['to'] = $to;
+			$this->Log['subject'] = '=?UTF-8?B?' . base64_encode($this->Subject) . '?=';
+			$this->Log['msg'] = $msg;
+			$this->Log['headers'] = $headers;	
+			
+			return $send;
+		}
+	}
+	
+	private function validation() {
+	
+		if(count($this->To) == 0) {
+			return false;
+		}
+		
+		if(!isset($this->From)) {
+			return false;
+		}
+
+		if(!isset($this->Subject)) {
+			return false;
+		}
+		
+		if(!isset($this->MessageText)) {
+			return false;
+		}
+
+		if(!isset($this->MessageHtml)) {
+			return false;
+		}		
+		
+		return true;
+	}
+	
+	private function getMessageText() {
+		
+		$message = 'This is a multi-part message in MIME format.'."\n\n"; 
+
+		$message .= '--'.$this->Separator."\n"; 
+		$message .= 'Content-Type: text/plain; charset="UTF-8"'."\n"; 
+		$message .= 'Content-Transfer-Encoding: 8bit'."\n\n"; 
+		$message .= $this->MessageText."\n\n"; 	
+
+		return $message;
+	}
+	
+	private function getMessageHtml() {
+		$message = '--'.$this->Separator."\n";
+		$message .= 'Content-Type: text/html; charset="iso-8859-1"'."\n"; 
+		$message .= 'Content-Transfer-Encoding: 8bit'."\n\n"; 
+		$message .= $this->MessageHtml."\n\n"; 
+		$message .= '--'.$this->Separator."\n"; 	
+		
+		return $message;
+	}
+	
+	private function getHeaders() {
+	
+		$from        = empty($this->FromName) ? $this->From : '"' . mb_encode_mimeheader($this->FromName) . '" <' . $this->From . '>';	
+	
+		if(isset($this->Reply)) {
+			$reply        = empty($this->ReplyName) ? $this->Reply : '"' . mb_encode_mimeheader($this->ReplyName) . '" <' . $this->Reply . '>';			
+		} else {
+			$reply        = $from;				
+		}
+		
+		$emails    = array
+		(
+			'From: ' . $from,
+			'Reply-To: ' . $reply
+		);		
+		
+		if(count($this->Cc) > 0) {
+			$arrayCc = array();
+			
+			foreach($this->Cc as $key => $value) {
+				$arrayCc = is_numeric($key) ? $value : '"' . mb_encode_mimeheader($key) . '" <' . $value . '>';
+			}
+			
+			$emails[] = 'Cc: '.implode(',', $arrayCc);	
+		}
+		
+		if(count($this->Bcc) > 0) {
+			$arrayBcc = array();
+			
+			foreach($this->Bcc as $key => $value) {
+				$arrayBcc = is_numeric($key) ? $value : '"' . mb_encode_mimeheader($key) . '" <' . $value . '>';
+			}
+			
+			$emails[] = 'Bcc: '.implode(',', $arrayBcc);			
+		}
+		
+		$headers    = array
+		(
+			'MIME-Version: 1.0',
+			'Content-Type: multipart/alternative; boundary="'.$this->Separator.'"',
+			'Date: ' . date('r', $_SERVER['REQUEST_TIME']),
+			'Message-ID: <' . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>'
+		);
+
+		$returns    = array
+		(
+			'Return-Path: ' . $from, 
+			'X-Originating-IP: ' . $_SERVER['SERVER_ADDR']
+		);		
+
+		return implode("\n", array_merge($headers, $emails, $returns));
+	}
+}
--- site/contact/send_contact.php
+++ site/contact/send_contact.php
@@ -14,7 +14,7 @@ require_once __DIR__ . '/../../conf/master.php';
 
 //-- envoie du formulaire à l'email de l'admin.
 
-$mail = $_POST['mail'];
+$email = $_POST['mail'];
 $sujet = $_POST['sujet'];
 $auteur = $_POST['nom'];
 $text = $_POST['text'];
@@ -23,13 +23,15 @@ $ip = $_SERVER["REMOTE_ADDR"];
 
 $admin_mail = 'staff@ewo.fr';
 
+$mail = new \conf\Mail();
+
 if (!empty($mail) && !empty($sujet) && !empty($auteur) && !empty($text)){
-	$headers ="From: Ewo<".$mail.">"."\n";
+	/*$headers ="From: Ewo<".$mail.">"."\n";
 	$headers .="Reply-To: ".$mail."\n";
 	$headers .='Content-Type: text/html; charset="iso-8859-1"'."\n"; 
-	$headers .='Content-Transfer-Encoding: 8bit';
+	$headers .='Content-Transfer-Encoding: 8bit';*/
 			
-	$message = "<html><head><title>EWO</title></head><body>
+	$messageHtml = "<h1>EWO</h1>
 							<p>Message venant du formulaire de contact</p>
 							<p>Sujet : $sujet</p>
 							<p>Auteur : $auteur</p>
@@ -37,8 +39,28 @@ if (!empty($mail) && !empty($sujet) && !empty($auteur) && !empty($text)){
 							<p>Ip : $ip</p>
 							<p>Message : $text</p>
 							</body></html>";
+							
+	$messageText = "EWO\n
+							Message venant du formulaire de contact\n
+							Sujet : $sujet\n
+							Auteur : $auteur\n
+							Mail : $mail\n
+							Ip : $ip\n
+							Message : $text\n";							
 
-	if(mail($admin_mail, '[Ewo] Formulaire de contact', $message, $headers)){
+	$mail->FromName = "Ewo";
+	$mail->From = $email;
+	$mail->To = 'leomaradan@gmail.com';
+	$mail->ToName = 'Ganesh';
+	$mail->Subject = '[Ewo] Formulaire de contact';
+	
+	if($mail->Send()) {
+		echo 'OK';
+	} else {
+		print_r($mail->Log());
+	}
+	
+	/*if(mail($admin_mail, '', $message, $headers)){
 		include(SERVER_ROOT . "template/header_new.php");
 			echo '<p>Votre message vient d\'être envoyé, les administrateurs du site feront au plus vite pour vous apporter une réponse.</p>';
 		include(SERVER_ROOT . "/template/footer_new.php");
@@ -46,7 +68,7 @@ if (!empty($mail) && !empty($sujet) && !empty($auteur) && !empty($text)){
 		include(SERVER_ROOT."/template/header_new.php");
 			echo '<p>Le message n\'a pu être envoyé</p>';
 		include(SERVER_ROOT . "/template/footer_new.php");
-	}
+	}*/
 }else{
 		$titre = "Erreur dans le message";
 		$text = "Il faut remplir les champs avant d'envoyer !";
