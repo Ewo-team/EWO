5d56ee0911d57ce73838ccf7f86e235693d97100
Ganesh <leomaradan@gmail.com>
2013-02-07 00:00:07 +0100

    Refactoring

---
+++ persos/creation/CreationPerso.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace persos\creation;
+
+/**
+ * Création d'un perso
+ *
+ * @author Leo
+ */
+class CreationPerso {
+    public $Mat = null;    
+    public $Nom;
+    public $Race;
+    public $UtilisateurId;
+    public $Gameplay;
+    public $Sexe;
+    public $Grade;
+    public $Galon;
+    public $Xp;
+
+    public function Save() {
+        
+    }
+}
+
+?>
