a5430d00e3f73908230e8ab3238363291acf8150
Kamule <benjamin.herbomez@gmail.com>
2013-03-29 15:51:24 +0100

    Update Effet.class.php

--- admin/gestion_actions/Effet.class.php
+++ admin/gestion_actions/Effet.class.php
@@ -1,12 +1,13 @@
 <?php
 
+use \admin\adminAdminDAO as admindAdminDAO ;
 
 class Effet {
 	public $type;
 	public $valeur;
 	
 	public function id() {
-		$conn = \admin\AdminDAO::getInstance();
+		$conn = AdminDAO::getInstance();
 		$id = $conn->SelectIdEffet($this->type, $this->valeur);
 		if(isset($id) && $id!=false) {
 			return $id['id'];
