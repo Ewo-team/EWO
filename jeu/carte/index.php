6a2a1ba154011c2678e304d704e05949243f2a38
Erase <saintpierre.b@gmail.com>
2013-11-22 23:27:00 +0100

    Carte(s) du monde EWO nouvelle version : compatibilité IE-Safari-Opera à confirmer

--- jeu/carte/index.php
+++ jeu/carte/index.php
@@ -90,8 +90,7 @@ $js->addScript('carte');
 
 <div class='centrage'>
 	<div id="boutons"></div>
-	<div id="map">
-		<embed id="emb" style="left:0;top:0;" type="image/svg+xml" src="svg_althian.php?hori=5&vert=3"></embed>
+	<div id="m_althian" class="getsvg" data-cible="svg_althian.php?hori=5&vert=3">
 	</div>
 </div>
 		
@@ -101,8 +100,8 @@ if(isset($_SESSION['persos']) && $_SESSION['persos']['inc'] > 0 && in_array(3, $
 <h2>C&eacute;lestia</h2>
 <div class='centrage'>
 	<div id="boutons"></div>
-	<div id="map">
-		<embed id="emb" style="left:0;top:0;" type="image/svg+xml" src="svg_celestia.php?hori=5&vert=3"></embed>
+	<div id="m_celestia" class="getsvg" data-cible="svg_celestia.php?hori=5&vert=3">
+		
 	</div>
 </div>
 <?php 
@@ -112,8 +111,7 @@ if(isset($_SESSION['persos']) && $_SESSION['persos']['inc'] > 0 && in_array(2, $
 <h2>Ciferis</h2>
 <div class='centrage'>
 	<div id="boutons"></div>
-	<div id="map">
-		<embed id="emb" style="left:0;top:0;" type="image/svg+xml" src="svg_ciferis.php?hori=5&vert=3"></embed>
+	<div id="m_ciferis" class="getsvg" data-cible="svg_ciferis.php?hori=5&vert=3">
 	</div>
 </div>
 <?php 
@@ -121,124 +119,11 @@ if(isset($_SESSION['persos']) && $_SESSION['persos']['inc'] > 0 && in_array(2, $
 ?>
 			
 			<script>
-				getElementsByClassName = function(cl) {
-					var retnode = [];
-					var myclass = new RegExp('\\b'+cl+'\\b');
-					var elem = this.getElementsByTagName('*');
-					for (var i = 0; i < elem.length; i++) {
-						var classes = elem[i].className;
-						if (myclass.test(classes)) retnode.push(elem[i]);
-					}
-					return retnode;
-				}; 
 				
-				var tabZooms = [0.5, 2];
-				var tabLimitZooms = [-2, 2];
-				var tabBoucliers = [];
-				var tabRaces = ['humain', 'paria', 'ange', 'demon'];
-				var svg;
-				var mouvement = 0;
-				function init(){
-					// On initialise notre svg
-					svg = getSvg();
-				
-					// DÃ©but RÃ©cupÃ©ration de tous les boucliers ************************//
-						var titreBoucliers = getAllBouclier();
-				
-						var elems = svg.getElementsByClassName('bouclier');
-						for(var i=0, y=elems.length;i<y;i++){
-							if(elems[i].getAttribute('id')){
-								tabBoucliers.push(new Array(titreBoucliers[elems[i].id],elems[i]));
-							}
-						}	
-					// Fin RÃ©cupÃ©ration de tous les boucliers *************************//
 
+				/*window.onload = function(){
 					
-				}
-				
-				function afficheCalque(elem){
-					if(elem.style.display == "none"){elem.style.display = "block";
-					}else{elem.style.display = "none";}
-				}
-
-				function zOom(type){
-					var op = '+';
-					var facZoom = tabZooms[1];
-					if(type=='-'){facZoom = tabZooms[0]; op = '-';}
-					if(op == '+'){mouvement++;}else{mouvement--;}
-					if(mouvement < tabLimitZooms[1] && mouvement > tabLimitZooms[0]){
-						var elems = svg.getElementsByTagName('*');
-						for(var i=0, y=elems.length; i<y;i++){
-							var tabAtr = elems[i].attributes;
-							for(var m=0, n=tabAtr.length;m<n;m++){
-								if(tabAtr[m].localName && tabAtr[m].nodeValue){
-									if(!isNaN(tabAtr[m].nodeValue)){
-										tabAtr[m].nodeValue = tabAtr[m].nodeValue*facZoom;
-									}
-								}
-							}
-							if(elems[i].tagName == 'text'){
-								if(elems[i].style.fontSize != ""){
-									var elem = elems[i].style.fontSize;
-									var px = elem.split('px');
-									if(op == '+'){
-										elems[i].style.fontSize = parseInt(parseInt(px[0])+7)+'px';
-									}else{
-										elems[i].style.fontSize = parseInt(parseInt(px[0])-7)+'px';
-									}
-								}else{
-									if(op == '+'){
-										elems[i].style.fontSize = '17px';
-									}else{
-										elems[i].style.fontSize = '3px';
-									}
-								}
-							}
-						
-						}
-					}else{
-						if(op == '+'){mouvement--;}else{mouvement++;}
-					}
-				}
-				
-				function afficheBoutons(){
-					var ret = '';
-					for(var i=0, y=tabBoucliers.length;i<y;i++){
-						ret += "<input type='button' value='"+tabBoucliers[i][0]+"' onclick='afficheCalque(svg.getElementById(\""+tabBoucliers[i][1].id+"\"))'>";
-					}
-					for(var i=0,y=tabRaces.length; i<y; i++){
-						ret += "<input type='button' value='"+tabRaces[i]+"' onclick='afficheCalque(svg.getElementsByClassName(\""+tabRaces[i]+"\")[0])'>";
-					}
-					ret += "<input type='button' value='Portes' onclick='afficheCalque(svg.getElementsByClassName(\"porte\")[0])'>";
-					ret += "<input type='button' value='Mes Personnages' onclick='afficheCalque(svg.getElementsByClassName(\"viseurs\")[0])'>";
-					//ret += "<input type='button' value='+' onclick='zOom(this.value)'>";
-					//ret += "<input type='button' value='-' onclick='zOom(this.value)'>";
-					document.getElementById('boutons').innerHTML += ret;
-				}
-				
-				
-				function Affiche(sens) {
-					if(sens > 0){zOom('+');
-					}else{zOom('-');}
-				}
-
-				function Molette(event){
-					var sens = 0;
-					if (!event){event = window.event;}
-					if (event.wheelDelta) {
-						sens =(window.opera)?-event.wheelDelta/120: event.wheelDelta/120; 
-					}else {sens=(event.detail)?-event.detail/3:sens;}
-					if (sens){
-						Affiche(sens);
-					}
-				}
-
-				window.onload = function(){
-					init();
-					afficheBoutons();
-					//if (svg.addEventListener){svg.addEventListener('DOMMouseScroll', Molette, false);}
-					//else{svg.onmousewheel =function(){Molette()};svg.onmousewheel = function(){Molette()};}
-				}
+				}*/
 				
 			</script>			
 </div>
--- js/jeu/carte.js
+++ js/jeu/carte.js
@@ -1,151 +1,85 @@
 "use strict";
 
-function layer_carte(id_layer) {
-    var layer = document.getElementById(id_layer).style.display;
-    if (layer === 'none') {
-        document.getElementById(id_layer).style.display = "block";
-    } else {
-        document.getElementById(id_layer).style.display = "none";
+var cloneToDoc = function(node,doc){
+    if (!doc){ 
+        doc = document;
     }
-}
-
-function layer_carte_all(id_layer) {
-    document.getElementById(id_layer).style.display = "block";
-}
-
-function layer_all(carte) {
-    layer_carte_all('carte_' + carte + '_grille');
-    layer_carte_all('carte_' + carte + '_viseur');
-    layer_carte_all('carte_' + carte + '_portes');
-    layer_carte_all('carte_' + carte + '_boucliers');
-    layer_carte_all('carte_' + carte + '_R1G0');
-    layer_carte_all('carte_' + carte + '_R1G4');
-    layer_carte_all('carte_' + carte + '_R1G5');
-    layer_carte_all('carte_' + carte + '_R4G0');
-    layer_carte_all('carte_' + carte + '_R4G4');
-    layer_carte_all('carte_' + carte + '_R4G5');
-    layer_carte_all('carte_' + carte + '_R3G0');
-    layer_carte_all('carte_' + carte + '_R3G4');
-    layer_carte_all('carte_' + carte + '_R3G5');
-    layer_carte_all('carte_' + carte + '_R2G0');
-    layer_carte_all('carte_' + carte + '_R2G4');
-    layer_carte_all('carte_' + carte + '_R2G5');
-} /* Ajout Coordonnées des cartes */
-
-function isChrome() {
-    var data = [{
-        string: navigator.userAgent,
-        subString: "Chrome"
-    }];
-    for (var i = 0; i < data.length; i++) {
-        var dataString = data[i].string;
-        var dataProp = data[i].prop;
-        if (dataString) {
-            if (dataString.indexOf(data[i].subString) !== -1) {
-                return true;
-            } else {
-                return false;
-            }
+    var clone = doc.createElementNS(node.namespaceURI,node.nodeName);
+    for (var i=0,len=node.attributes.length;i<len;++i){
+        var a = node.attributes[i];
+        if (/^xmlns\b/.test(a.nodeName)){
+            continue;
         }
+        clone.setAttributeNS(a.namespaceURI,a.nodeName,a.nodeValue);
     }
-}
-
-function viewportOff(el) {
-    var valueT = 0,
-        valueL = 0,
-        docBody = document.body;
-    var element = el;
-    do {
-        valueT += element.offsetTop || 0;
-        valueL += element.offsetLeft || 0;
-        if (element.offsetParent === docBody && element.style.position === 'absolute') {
-            break;
-        }
-    } while (element = element.offsetParent);
-    if (!isChrome()) {
-        element = el;
-        do {
-            if (element !== docBody) {
-                valueT -= element.scrollTop || 0;
-                valueL -= element.scrollLeft || 0;
-            }
-        } while (element = element.parentNode);
-    } else {
-        valueL = valueL - document.body.scrollLeft;
-        valueT = valueT - document.body.scrollTop;
+    for (var i=0,len=node.childNodes.length;i<len;++i){
+        var c = node.childNodes[i];
+        clone.insertBefore(c.nodeType==1 ? cloneToDoc(c,doc) : doc.createTextNode(c.nodeValue), null);
     }
-    return {
-        'left': Math.round(valueL),
-        'top': Math.round(valueT)
-    };
-}
+    return clone;
+};
 
-function findPos(el) {
-    if (el.offsetParent) {
-        var coord = viewportOff(el);
-        return {
-            'x': coord.left,
-            'y': coord.top
-        };
+var _lib = {
+    'humain'    :   'Humains', 
+    'ange'      :   'Anges',
+    'demon'     :   'Démons',
+    'bouclier'  :   'Boucliers',
+    'porte'     :   'Portes',
+    'viseurs'   :   'Mes personnages'
+ };
+
+var _d = {
+    'm_althian' : {
+        'x' : {'d' : 5, 'm' : 100},
+        'y' : {'d' : 3, 'm' : 100}
+    },
+    'm_celestia' : {
+        'x' : {'d' : 5, 'm' : 50},
+        'y' : {'d' : 3, 'm' : 100}
+    },
+    'm_ciferis' : {
+        'x' : {'d' : 5, 'm' : 50},
+        'y' : {'d' : 3, 'm' : 0}
     }
-    return false;
-}
+};
 
-function razStatut() {
-    if (document.getElementById(this.id + '_coord')) {
-        document.getElementById(this.id + '_coord').style.visibility = 'hidden';
-    }
-}
+$('.getsvg').each(function(){
+    var o_p = $(this);      
+    $.ajax({
+        type    :   'get',
+        url     :   o_p.attr('data-cible'),
+        dataType:   'xml',
+        success :   function(xml){
+            var c           = cloneToDoc(xml.documentElement);
+            window.svgRoot  = c;
+            document.getElementById(o_p.attr('id')).appendChild(c);
+            delete window.svgRoot;
+            var o_svg   = o_p.find('svg').css({'display':'block','margin':'0 auto'});
+            var _e      = $('g', o_svg);
+            $.each(_e, function(){
+                var $this = $(this);
+                if($this.attr('class') != 'noir'){
+                    var clas    =   $this.attr('class');
+                    o_p.prepend("<input type='button' class='"+clas+"' value='"+(_lib[clas] ? _lib[clas] : clas)+"' />");
+                }
+            });
+            o_p.find('input[type=button]').click(function(){
+                var calque = $('g[class='+$(this).attr('class')+']', o_p);
+                calque.css('display') == 'none' ? calque.show() : calque.hide()
+            }).last().after('<div class="lib_loc">Position X : <span class="px">DTC</span>  |  Position Y : <span class="py">DTC</span></div>');
 
-function setRes() {
-    if (document.getElementById(arguments[0] + '_coord') && document.getElementById(arguments[0] + '_coordX') && document.getElementById(arguments[0] + '_coordY')) {
-        document.getElementById(arguments[0] + '_coord').style.visibility = 'visible';
-        document.getElementById(arguments[0] + '_coordX').innerHTML = arguments[1];
-        document.getElementById(arguments[0] + '_coordY').innerHTML = arguments[2];
-    }
-}
-
-function getParamPlan(el) {
-    switch (el) {
-    case 'fond_carte_terre':
-        return {
-            'rx': Math.floor(arguments[1] / 2.33 - 150),
-            'ry': Math.floor(arguments[2] / 1.33 - 150)
-        };
-    case 'fond_carte_paradis':
-        return {
-            'rx': Math.floor(arguments[1] / 4 - 50),
-            'ry': Math.floor(arguments[2] / 4 - 100)
-        };
-    case 'fond_carte_enfer':
-        return {
-            'rx': Math.floor(arguments[1] / 4 - 50),
-            'ry': Math.floor(arguments[2] / 4)
-        };
-    }
-}
-
-function setCoordPlan(e) {
-    var ev = e || window.event;
-    var pos = findPos(this);
-    var diffx = ev.clientX - pos.x;
-    var diffy = ev.clientY - pos.y;
-    var calc = getParamPlan(this.id, diffx, diffy);
-    calc.ry < 0 ? calc.ry = Math.abs(calc.ry) : calc.ry = '-' + calc.ry;
-    setRes(this.id, calc.rx, calc.ry);
-}
-if (document.getElementById('fond_carte_terre')) {
-    var dct = document.getElementById('fond_carte_terre');
-    dct.onmousemove = setCoordPlan;
-    dct.onmouseout = razStatut;
-}
-if (document.getElementById('fond_carte_paradis')) {
-    var dcp = document.getElementById('fond_carte_paradis');
-    dcp.onmousemove = setCoordPlan;
-    dcp.onmouseout = razStatut;
-}
-if (document.getElementById('fond_carte_enfer')) {
-    var dce = document.getElementById('fond_carte_enfer');
-    dce.onmousemove = setCoordPlan;
-    dce.onmouseout = razStatut;
-}
+            o_svg.mousemove(function(e){
+                o_p.find('.lib_loc').css('visibility', 'visible');
+                var c = {
+                    x: Math.floor((e.pageX - $(this).offset().left) / _d[o_p.attr('id')]['x']['d'] - _d[o_p.attr('id')]['x']['m']),
+                    y: Math.floor((e.pageY - $(this).offset().top) / _d[o_p.attr('id')]['y']['d'] - _d[o_p.attr('id')]['y']['m'])
+                };
+                c.y = (c.y <= 0) ? Math.abs(c.y) : '-' + c.y;
+                o_p.find('.lib_loc .px').html(c.x);
+                o_p.find('.lib_loc .py').html(c.y);
+            }).mouseout(function(){
+                o_p.find('.lib_loc').css('visibility', 'hidden');
+            });
+        }   
+    });
+});
